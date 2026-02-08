c704497980c1c276cdfa3ce33ce4fdd87942cd01
adolfogomes <Adolfo.Gomes@ibm.com>
2026-02-08 13:49:04 -0300

    feat: improvements in button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,18 @@
 import type { MouseEventHandler, ReactNode } from "react"
 import styles from './Button.module.scss';
+import { type ComponentPropsWithoutRef } from 'react';
 
-type Props = {
+
+interface ButtonProps extends ComponentPropsWithoutRef<'button'> {
   children: ReactNode,
   onClick: MouseEventHandler<HTMLButtonElement>,
-  type: 'primary' | 'secondary' | 'danger',
-  style?: Record<string, string>,
+  kind: 'primary' | 'secondary' | 'danger' | 'ghost',
 }
 
-export const Button = ({children, onClick, type, style}:Props) => {
+export const Button = ({children, onClick, kind, ...props}:ButtonProps) => {
 
   return (
-    <button type="button" onClick={onClick} className={`${styles[type]}`} style={style}>
+    <button type="button" onClick={onClick} className={`${styles[kind]}`} {...props}>
       {children}
     </button>
   )
