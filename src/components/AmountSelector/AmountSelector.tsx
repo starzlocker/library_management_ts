34f90f14b731f93f2575829bfe201361df26e973
adolfogomes <Adolfo.Gomes@ibm.com>
2026-02-13 08:38:45 -0300

    feat: AmountSelector component

--- src/components/AmountSelector/AmountSelector.tsx
+++ src/components/AmountSelector/AmountSelector.tsx
@@ -1,18 +1,35 @@
-import { AddIcon } from "../AddIcon"
-import { Button } from "../Button/Button"
-import { CloseIcon } from "../CloseIcon"
-import styles from './_.module.scss'
+import type { ComponentPropsWithoutRef } from "react";
+import { AddIcon } from '../AddIcon';
+import { Button } from '../Button/Button';
+import { CloseIcon } from '../CloseIcon';
+import { MinusIcon } from '../MinusIcon';
+import styles from './_.module.scss';
 
-type Props = {
-  amount: number
+interface AmountSelectorProps extends ComponentPropsWithoutRef<'div'> {
+  amount: number;
+  onIncrement: () => void;
+  onDecrement: () => void;
 }
 
-export const AmountSelector = ({amount}:Props) => {
-return (
-  <div className={styles['amount']}>
-      <Button className={styles['remove-button']} onClick={() => {}} kind="ghost"><CloseIcon size={11.4}/></Button>
-      <input type="number" min={0} disabled value={amount}/>
-      <Button className={styles['add-button']} onClick={() => {}} kind="ghost"><AddIcon /></Button>
+export const AmountSelector = ({ amount, onIncrement, onDecrement, className, ...props }: AmountSelectorProps) => {
+  return (
+    <div className={`${styles['amount']} ${className || ''}`} {...props}>
+      <Button
+        className={styles['remove-button']}
+        onClick={onDecrement}
+        kind='ghost'
+      >
+        {amount === 1 ? <CloseIcon size={18} color='red'/> : <MinusIcon size={18} color='red'/>}
+      </Button>
+
+      <input type='text' pattern='/[0-9]*/' value={amount} disabled />
+      <Button
+        className={styles['add-button']}
+        onClick={() => onIncrement()}
+        kind='ghost'
+      >
+        <AddIcon size={18} color='blue'/>
+      </Button>
     </div>
-  )
-}
+  );
+};
