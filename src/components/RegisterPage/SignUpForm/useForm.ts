cb60175c9c581cd109c1f76627a4126d67150f8f
adolfogomes <Adolfo.Gomes@ibm.com>
2026-02-15 21:36:38 -0300

    checkpoint: useForm

---
+++ src/components/RegisterPage/SignUpForm/useForm.ts
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+type fields = {
+  [key: string]: unknown;
+};
+
+export const useForm = () => {
+  const [errorMap, setErrorMap] = useState({} as Record<string, Array<string>>);
+
+  const validate = (fields: fields) => {
+    for(const [k, v] of Object.entries(fields)) {
+      console.log(k, v)
+    }
+  }
+
+  const isValid = () => {
+    if(Object.keys(errorMap)) return false;
+
+    return true;
+  };
+
+  const addErrorToField = (field:string, error: string) => {
+    let newValue = null;
+    if (field in errorMap) {
+      newValue = [...errorMap[field], error];
+    } else {
+      newValue = [error];
+    }
+    setErrorMap(prev => ({
+      ...prev,
+      [field]: newValue
+    }))
+  }
+
+  return {
+    errorMap,
+    addErrorToField,
+    isValid
+  }
+};
