9195d33c8af11e0a974c9bfb17b5b990be8c74f0
adolfogomes <Adolfo.Gomes@ibm.com>
2026-02-16 00:28:41 -0300

    refactor: styling fixes

--- src/components/misc/BookListHeading.tsx
+++ src/components/misc/BookListHeading.tsx
@@ -5,6 +5,9 @@ export const BookListHeading = () => {
       alignItems: 'baseline',
       gap: '8px',
       marginBottom: '1rem',
+      maxWidth: '1200px',
+      width: '100%',
+      margin: 'auto',
     }}><span style={{color: '#e3d916'}}>★</span> Mais Populares <span style={{color: '#e3d916'}}>★</span></h2>
 
   )
