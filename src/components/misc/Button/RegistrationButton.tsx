b45f5008f35a7487b0f51caa8dc84e1beb116260
adolfogomes <Adolfo.Gomes@ibm.com>
2026-03-26 18:05:18 -0300

    checkpoint: ongoing

--- src/components/misc/Button/RegistrationButton.tsx
+++ src/components/misc/Button/RegistrationButton.tsx
@@ -3,16 +3,16 @@ import styles from "./Button.module.scss";
 import { type ComponentPropsWithoutRef } from "react";
 
 interface ButtonProps extends ComponentPropsWithoutRef<"button"> {
-  children?: ReactNode;
-  onClick?: MouseEventHandler<HTMLButtonElement>;
-  kind?: "primary" | "secondary" | "danger" | "ghost";
+  children: ReactNode;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  kind: "primary" | "secondary" | "danger" | "ghost";
   ref?: ForwardedRef<HTMLButtonElement>;
 }
 
 export const Button = ({
   children,
   onClick,
-  kind='primary',
+  kind,
   ref,
   className,
   ...props
--- src/components/misc/UserLoginModal/UserRegistrationForm.tsx
+++ src/components/misc/UserLoginModal/UserRegistrationForm.tsx
@@ -1,52 +1,104 @@
-import { Button } from "../Button/Button";
-import styles from "./_.module.scss";
-
+import { useState } from 'react';
+import { Button } from '../Button/Button';
+import styles from './_.module.scss';
+import z from 'zod';
+import type { KeyOf } from 'node_modules/zod/v4/core/util.d.cts';
 type Props = {
   openLoginForm: () => void;
 };
 
+const validateSchema = z.strictObject({
+  firstName: z
+    .string({ error: 'Preencha o seu primeiro nome' })
+    .min(1, { error: 'Preencha o seu primeiro nome' }),
+  lastName: z
+    .string({ error: 'Preencha seu último nome' })
+    .min(1, { error: 'Preencha o seu primeiro nome' }),
+  cpf: z
+    .string({ error: 'Preencha com seu CPF (pode ser de mentirinha)' })
+    .regex(/\d{11}/, { error: 'CPF deve ser composto por 11 digitos' }),
+  email: z.email({ error: 'Preencha seu email' }),
+  password: z
+    .string({ error: 'Preencha sua senha' })
+    .min(8, { error: 'A senha deve ter 8 digitos' }),
+});
+
+type FormErrors = {
+  firstName?: Array<string>;
+  lastName?: Array<string>;
+  cpf?: Array<string>;
+  email?: Array<string>;
+  password?: Array<string>;
+};
+
+const formFields: Array<{
+  id: number;
+  field: KeyOf<FormErrors>;
+  text: string;
+}> = [
+  { id: 1, field: 'firstName', text: 'Primeiro Nome' },
+  { id: 2, field: 'lastName', text: 'Sobrenome' },
+  { id: 3, field: 'cpf', text: 'CPF' },
+  { id: 5, field: 'email', text: 'Email' },
+  { id: 6, field: 'password', text: 'Password' },
+];
+
 export const UserRegistrationForm = ({ openLoginForm }: Props) => {
+  const [formErrors, setFormErrors] = useState<FormErrors>({});
+
+  const onSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const formDataJson = Object.fromEntries(formData.entries());
+    const result = validateSchema.safeParse(formDataJson);
+    if (!result.success) {
+      const errors = z.treeifyError(result.error);
+      const output: FormErrors = {};
+      for (const k in errors.properties) {
+        const typedKey = k as KeyOf<FormErrors>;
+        output[typedKey] = errors.properties?.[typedKey]?.errors;
+      }
+
+      setFormErrors(output ?? {});
+    }
+
+    console.log(result.data);
+  };
+
   return (
-    <section className={styles["registration-container"]}>
+    <section className={styles['registration-container']}>
       <h2>Cadastro</h2>
       <p>Preencha os campos abaixos para se cadastrar:</p>
-      <form>
-        <div className={styles["input-field"]}>
-          <label htmlFor="firstname">Primeiro Nome</label>
-          <input type="firstname" id="firstname" required />
-        </div>
-        <div className={styles["input-field"]}>
-          <label htmlFor="lastname">Sobrenome</label>
-          <input type="lastname" id="lastname" required />
-        </div>
-        <div className={styles["input-field"]}>
-          <label htmlFor="cpf">CPF</label>
-          <input type="cpf" id="cpf" required />
-        </div>
-        <div className={styles["input-field"]}>
-          <label htmlFor="gov_password">Senha do GOV</label>
-          <input type="gov_password" id="gov_password" required />
-        </div>
-        <div className={styles["input-field"]}>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" required />
-        </div>
-        <div className={styles["input-field"]}>
-          <label htmlFor="password">Senha</label>
-          <input type="password" id="password" required />
-        </div>
+      <form onSubmit={onSubmit}>
+        {formFields.map((field) => (
+          <div key={field.id}>
+            <div
+              className={styles['input-field']}
+              data-error={formErrors[field.field] ? true : undefined}
+            >
+              <label htmlFor={field.field}>{field.text}</label>
+              <input type={field.field} id={field.field} name={field.field} />
+            </div>
+            {formErrors[field.field] &&
+              formErrors[field.field]?.map((error) => (
+                <p className={styles['error-text']} key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
+        ))}
+        <Button
+          type='submit'
+          kind='primary'
+          className={styles['return-button']}
+        >
+          Cadastrar
+        </Button>
       </form>
       <Button
-        kind="primary"
-        onClick={() => {}}
-        className={styles["return-button"]}
-      >
-        Cadastrar
-      </Button>
-      <Button
-        kind="secondary"
+        kind='secondary'
         onClick={() => openLoginForm()}
-        className={styles["return-button"]}
+        className={styles['return-button']}
       >
         Voltar
       </Button>
